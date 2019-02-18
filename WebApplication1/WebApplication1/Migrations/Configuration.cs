7c7f45922cdde2f52dc043d6d5ba4d8c0369a85b
Rolando Naranjo <rolando.naranjo92@yahoo.com>
2019-02-18 13:54:22 -0800

    Initial Migration

---
+++ WebApplication1/WebApplication1/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace WebApplication1.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<VetsEvents.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(VetsEvents.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
