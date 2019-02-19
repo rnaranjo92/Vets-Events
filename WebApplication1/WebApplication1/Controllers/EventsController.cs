bbf34051c3570503200546940b652857c5a27784
Rolando Naranjo <rolando.naranjo92@yahoo.com>
2019-02-18 16:25:47 -0800

    Add EventsController, EventsFormViewModel, Create View and add "Add an Event" in the Nav Bar

---
+++ WebApplication1/WebApplication1/Controllers/EventsController.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using VetsEvents.Models;
+using VetsEvents.ViewModels;
+
+namespace VetsEvents.Controllers
+{
+    public class EventsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EventsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        public ActionResult Create()
+        {
+            var viewModel = new EventFormViewModel
+            {
+                EventTypes = _context.EventTypes.ToList()
+            };
+
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/ViewModels/EventFormViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using VetsEvents.Models;
+
+namespace VetsEvents.ViewModels
+{
+    public class EventFormViewModel
+    {
+        public string Venue { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public int EventType { get; set; }
+        public IEnumerable<EventType> EventTypes { get; set; }
+    }
+}
