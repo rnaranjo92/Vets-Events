249896bbfc5af34d6273e346d945461835d1cc62
Rolando Naranjo <rolando.naranjo92@yahoo.com>
2019-03-12 20:55:14 -0700

    Refactoring my Code(To be continued.)

--- WebApplication1/WebApplication1/App_Start/BundleConfig.cs
+++ WebApplication1/WebApplication1/App_Start/BundleConfig.cs
@@ -7,12 +7,16 @@ namespace VetsEvents
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.Add(new ScriptBundle("~/bundles/app").Include(
+                "~/Scripts/app/app.js"
+                ));
             bundles.Add(new ScriptBundle("~/bundles/lib").Include(
                         "~/Scripts/jquery-{version}.js",
                         "~/Scripts/underscore-min.js",
                         "~/Scripts/moment.js",
                         "~/Scripts/bootstrap.js",
-                      "~/Scripts/bootbox.min.js"
+                      "~/Scripts/bootbox.min.js",
+                      "~/Scripts/app.js"
                       ));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
---
+++ WebApplication1/WebApplication1/Scripts/app/app.js
@@ -0,0 +1,39 @@
+﻿var EventsController = function () {
+    var init = function () {
+        $(".js-toggle-attendance").click(toggleAttendance); 
+    };
+    var toggleAttendance = function (e) {
+        var button = $(e.target);
+        if (button.hasClass("btn-default")) {
+            $.post("/api/attendances", { eventId: button.attr("data-event-id") })
+                .done(function () {
+                    button
+                        .removeClass("btn-default")
+                        .addClass("btn-info")
+                        .text("Going");
+                })
+                .fail(function () {
+                    alert("Something failed!");
+                });
+        } else {
+            $.ajax({
+                url: "/api/attendances/" + button.attr("data-event-id"),
+                method: "DELETE"
+            })
+                .done(function () {
+                    button
+                        .removeClass("btn-info")
+                        .addClass("btn-default")
+                        .text("Going?");
+                })
+                .fail(function () {
+                    alert("Something failed");
+                });
+        }
+    };
+
+    return {
+        init:init
+    }
+}();
+
